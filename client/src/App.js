57566ca49eee80f1ff84ebed33ed509c4b55dfca
ColemanMatthew-FS <MRColeman1@student.fullsail.edu>
2022-05-23 12:49:22 -0400

    Slight adjustments

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Results from './pages/Results'
 import Details from './pages/Details'
